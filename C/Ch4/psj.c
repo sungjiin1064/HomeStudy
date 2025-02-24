775d70e5d687f727b4af87acdd6a848aab6878be
sungjiin1064 <psj10645001@gmail.com>
2025-02-25 02:41:54 +0900

    (2025-02-25))

--- C/Ch4/psj.c
+++ C/Ch4/psj.c
@@ -18,7 +18,7 @@ int main()
 	printf("몬스터의 피가 %d만큼 남음\n", (mHP - A * 4) < 0 ? 0 : (mHP - A * 4));
 	printf("몬스터를 공격하여 %d만큼 데미지를줌\n", A);
 	printf("몬스터의 피가 %d만큼 남음\n", (mHP - A * 5) < 0 ? 0 : (mHP - A * 5));
-
+	
 
 }
 
